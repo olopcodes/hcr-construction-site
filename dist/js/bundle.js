baf9c2fdaa8cb1dbf5c21de3ca161fffe4fb5140
olopcodes <olopcodes@gmail.com>
2021-12-28 22:18:22 -0500

    last commit before merge with focus

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,9 +1,9 @@
 "use strict";
 
-console.log("app");
-"use strict";
-
 console.log("hey"); // change focus state when get a quote is clicked
 // element.focus()
 // animate form on focus
 // animate header on scroll
+"use strict";
+
+console.log("app");
