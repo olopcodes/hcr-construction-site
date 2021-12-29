1e1194a65e1a5cc4ad1df62aca297cf1dcde9584
olopcodes <olopcodes@gmail.com>
2021-12-29 16:50:23 -0500

    added most of the logic, revisit when section below hero is done

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -22,4 +22,28 @@ regularForm.addEventListener("focusout", function (e) {
 "use strict";
 
 // from observer
-console.log("hello again observer");
+console.log("hello again observer branch");
+var header = document.querySelector(".header"); //  this will be the content after the hero section
+
+var targetToObserve = document.querySelector(".home-hero");
+
+var changeBgOnScroll = function changeBgOnScroll(entries, obs) {
+  entries.forEach(function (entry) {
+    // if entry is not in the viewport
+    if (!entry.isIntersecting) return; // stop observing
+    // obs.unobserve(entry.target);
+    // let me know when target intersects
+
+    console.log("target is in the viewport");
+    console.log(entry.intersectionRatio); // do this to the element
+
+    header.classList.add("scroll");
+  });
+};
+
+var options = {
+  rootMargin: "70px",
+  threshold: 0.4
+};
+var observer = new IntersectionObserver(changeBgOnScroll, options);
+observer.observe(targetToObserve);
--- src/js/observer.js
+++ src/js/observer.js
@@ -1,2 +1,30 @@
 // from observer
-console.log("hello again observer");
+console.log("hello again observer branch");
+const header = document.querySelector(".header");
+//  this will be the content after the hero section
+const targetToObserve = document.querySelector(".home-hero");
+
+const changeBgOnScroll = (entries, obs) => {
+  entries.forEach((entry) => {
+    // if entry is not in the viewport
+    if (!entry.isIntersecting) return;
+
+    // stop observing
+    // obs.unobserve(entry.target);
+
+    // let me know when target intersects
+    console.log("target is in the viewport");
+    console.log(entry.intersectionRatio);
+
+    // do this to the element
+    header.classList.add("scroll");
+  });
+};
+
+const options = {
+  rootMargin: "70px",
+  threshold: 0.4,
+};
+
+const observer = new IntersectionObserver(changeBgOnScroll, options);
+observer.observe(targetToObserve);
