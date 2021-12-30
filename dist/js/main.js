d9c414e770736580317a68fdd32f7f8a07b413ac
olopcodes <olopcodes@gmail.com>
2021-12-29 21:21:50 -0500

    added html for home-best section

---
+++ dist/js/main.js
@@ -0,0 +1,20 @@
+// change focus state when get a quote is clicked
+const getQuoteBtn = document.getElementById("get-quote");
+// animate form on focus
+getQuoteBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.getElementById("hero-name-input").focus();
+});
+
+const regularForm = document.querySelector(".form-regular");
+regularForm.addEventListener("focusin", (e) => {
+  e.target.previousElementSibling.classList.add("focus");
+});
+
+regularForm.addEventListener("focusout", (e) => {
+  if (e.target.value === "") {
+    e.target.previousElementSibling.classList.remove("focus");
+  }
+});
+
+// animate header on scroll
