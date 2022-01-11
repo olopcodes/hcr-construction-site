072ae985d1d9cf5c208833eb9388ae1700f3b6db
olopcodes <olopcodes@gmail.com>
2022-01-10 22:17:32 -0500

    finished the tab coponent

--- src/js/tab.js
+++ src/js/tab.js
@@ -1,1 +1,14 @@
-
+const tabsWrapper = document.querySelector(".home-works__tabs");
+const works = document.querySelectorAll(".home-works__work");
+const tabs = tabsWrapper.querySelectorAll("button");
+tabsWrapper.addEventListener("click", (e) => {
+  for (let t of tabs) {
+    t.classList.remove("active-tab");
+  }
+  e.target.closest("button").classList.add("active-tab");
+  for (let w of works) {
+    w.classList.remove("active-work");
+  }
+  const work = document.getElementById(e.target.closest("button").dataset.id);
+  work.classList.add("active-work");
+});
