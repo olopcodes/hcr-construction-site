170eca256259a9ac5be317555c03624811b46be5
olopcodes <olopcodes@gmail.com>
2022-01-17 18:31:49 -0500

    fixed the dataset json

--- dist/js/projects-page.js
+++ dist/js/projects-page.js
@@ -1,3 +1,11 @@
-// const img = document.querySelector(".projects-section__images img");
-// console.log(JSON.parse(img.dataset.sources).srcSmall);
-// console.log(img.dataset.img);
+"use strict";
+
+var imgs = document.querySelectorAll(".projects-section__images img"); // console.log(JSON.parse(img.dataset.sources).srcSmall);
+
+console.log(imgs); //   console.log(sources);
+
+imgs.forEach(function (img) {
+  // console.log(JSON.parse(img.dataset));
+  // console.log(JSON.parse('{"name":"John", "age":"30", "city":"New York"}'));
+  console.log(JSON.parse(img.dataset.sources));
+});
--- src/js/projects-page.js
+++ src/js/projects-page.js
@@ -1,3 +1,11 @@
-// const img = document.querySelector(".projects-section__images img");
+const imgs = document.querySelectorAll(".projects-section__images img");
 // console.log(JSON.parse(img.dataset.sources).srcSmall);
-// console.log(img.dataset.img);
+console.log(imgs);
+
+//   console.log(sources);
+
+imgs.forEach((img) => {
+  // console.log(JSON.parse(img.dataset));
+  // console.log(JSON.parse('{"name":"John", "age":"30", "city":"New York"}'));
+  console.log(JSON.parse(img.dataset.sources));
+});
