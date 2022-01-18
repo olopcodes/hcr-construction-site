5bc6cca418cc4c984e0e3796b671634e8118baed
olopcodes <olopcodes@gmail.com>
2022-01-17 22:15:25 -0500

    styled and built modal

--- dist/js/projects-page.js
+++ dist/js/projects-page.js
@@ -1,11 +1,25 @@
 "use strict";
 
-var imgs = document.querySelectorAll(".projects-section__images img"); // console.log(JSON.parse(img.dataset.sources).srcSmall);
-
-console.log(imgs); //   console.log(sources);
-
+var imgs = document.querySelectorAll(".projects-section__images img");
+var modal = document.querySelector("#modal");
+var modalBtn = document.getElementById("modal-btn");
 imgs.forEach(function (img) {
-  // console.log(JSON.parse(img.dataset));
-  // console.log(JSON.parse('{"name":"John", "age":"30", "city":"New York"}'));
-  console.log(JSON.parse(img.dataset.sources));
+  img.addEventListener("click", function (e) {
+    var imgSrc;
+
+    if (window.innerWidth < 800) {
+      imgSrc = JSON.parse(e.target.dataset.sources).one;
+    } else {
+      imgSrc = JSON.parse(e.target.dataset.sources).two;
+    }
+
+    modal.querySelector("img").src = imgSrc;
+    modal.classList.add("show");
+    console.log();
+    modalBtn.href = e.target.closest(".projects-section__project").id;
+  });
+});
+modalBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  modal.classList.remove("show");
 });
--- src/js/projects-page.js
+++ src/js/projects-page.js
@@ -1,11 +1,24 @@
 const imgs = document.querySelectorAll(".projects-section__images img");
-// console.log(JSON.parse(img.dataset.sources).srcSmall);
-console.log(imgs);
-
-//   console.log(sources);
+const modal = document.querySelector("#modal");
+const modalBtn = document.getElementById("modal-btn");
 
 imgs.forEach((img) => {
-  // console.log(JSON.parse(img.dataset));
-  // console.log(JSON.parse('{"name":"John", "age":"30", "city":"New York"}'));
-  console.log(JSON.parse(img.dataset.sources));
+  img.addEventListener("click", (e) => {
+    let imgSrc;
+    if (window.innerWidth < 800) {
+      imgSrc = JSON.parse(e.target.dataset.sources).one;
+    } else {
+      imgSrc = JSON.parse(e.target.dataset.sources).two;
+    }
+    modal.querySelector("img").src = imgSrc;
+    modal.classList.add("show");
+    console.log();
+
+    modalBtn.href = e.target.closest(".projects-section__project").id;
+  });
+});
+
+modalBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  modal.classList.remove("show");
 });
